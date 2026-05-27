74b4ccf0c996cdc7daadc14e46b8518dca41ae61
yjailbir <nkudashko@mail.ru>
2026-05-27 18:53:52 +0300

    initial commit 2

---
+++ src/main/java/ru/yjailbir/staticfilesserver/StaticFilesServerApplication.java
@@ -0,0 +1,13 @@
+package ru.yjailbir.staticfilesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticFilesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaticFilesServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yjailbir/staticfilesserver/StaticFilesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yjailbir.staticfilesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticFilesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
